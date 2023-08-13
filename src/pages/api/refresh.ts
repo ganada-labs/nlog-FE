0305adf9bfe1f3920e742638bb4ce13eda16e3d7
dev2820 <second28200@gmail.com>
2023-08-13 14:15:32 +0900

    update/ refresh 토큰 내용 채움

--- src/pages/api/refresh.ts
+++ src/pages/api/refresh.ts
@@ -1,5 +1,17 @@
+import { nlogAPI } from "src/utils/nlog-api";
+
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function refresh(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Hello, world!" });
+export default async function refresh(req: NextApiRequest, res: NextApiResponse) {
+  const refreshToken = req.cookies["refresh_token"]; // 쿠키 이름으로 읽기
+  console.log(7, refreshToken, req.cookies);
+  /**
+   * api에 refresh 요청
+   */
+  const { data: newToken } = await nlogAPI.get("/auth/refresh", {
+    headers: {
+      Authorization: `Bearer ${refreshToken}`,
+    },
+  });
+  res.status(200).json({ newToken });
 }
