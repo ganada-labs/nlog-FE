8a2fd3d4522a1684b9d949985f40b29f8ba667c5
dev2820 <second28200@gmail.com>
2023-08-13 17:05:40 +0900

    refactor/ remove code

--- src/pages/api/refresh.ts
+++ src/pages/api/refresh.ts
@@ -1,23 +1,23 @@
-import { nlogAPI } from "src/utils/nlog-api";
+// import { nlogAPI } from "src/utils/nlog-api";
 
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function refresh(req: NextApiRequest, res: NextApiResponse) {
-  const refreshToken = req.cookies["refresh_token"]; // 쿠키 이름으로 읽기
+export default function refresh(req: NextApiRequest, res: NextApiResponse) {
+  // const refreshToken = req.cookies["refresh_token"]; // 쿠키 이름으로 읽기
 
   /**
    * api에 refresh 요청
    */
-  const { data: newTokens } = await nlogAPI.get("/auth/refresh", {
-    headers: {
-      Authorization: `Bearer ${refreshToken}`,
-    },
-  });
+  // const { data: newTokens } = await nlogAPI.get("/auth/refresh", {
+  //   headers: {
+  //     Authorization: `Bearer ${refreshToken}`,
+  //   },
+  // });
 
-  const { accessToken: newAccessToken, refreshToken: newRefreshToken } = newTokens;
+  // const { accessToken: newAccessToken, refreshToken: newRefreshToken } = newTokens;
   res.setHeader("Set-Cookie", [
-    `refresh_token=${newRefreshToken}; Domain=.new-blog.store; HttpOnly; MaxAge=0`,
-    `refresh_token=${newRefreshToken}; Domain=.new-blog.store; HttpOnly; Path=/`,
+    "refresh_token=;Max-Age=0",
+    // `refresh_token=${newRefreshToken}; Domain=.new-blog.store; HttpOnly; Path=/`,
   ]);
-  res.status(200).json(newAccessToken);
+  res.status(200).json("test");
 }
